60a6f30e23fa39e52cf99cae6547d22b02bf0a4b
EmanuelPuertas <emapuertas16@gmail.com>
2023-10-19 19:07:51 -0300

    agregue uno nuevo

---
+++ app.js
@@ -0,0 +1,22 @@
+/*let nombre = "Ema";
+let apellido = "Puertas";
+let sueldoActual = 100000.00;
+let porcentaAumento = 20;
+
+let calcularAumento = (sueldoActual*porcentaAumento)/100;
+let nuevoSueldo = calcularAumento + sueldoActual;
+
+console.log("Esimado "+nombre+" "+apellido);
+console.log("su sueldo actual es de "+sueldoActual);
+console.log("el porcentaje de aumento es: "+porcentaAumento);
+console.log("Aumento es de: "+calcularAumento);
+console.log("su nuevo sueldo es :"+nuevoSueldo);
+*/
+let numero1 = 10;
+let numero2 = 20;
+
+let esMayor = numero1 > numero2;
+let esMenor = numero1 < numero2;
+
+console.log("El numero1 es mayor? "+esMayor);
+console.log("El numero1 es menor? "+esMenor);
